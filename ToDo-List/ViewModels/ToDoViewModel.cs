2803b04b0e506da5204a19c85eac9c6a6a5b4ecb
Дмитрий Андреев <da616142@gmail.com>
2025-02-04 23:17:24 +0300

    Добавлена возможность добавлять задачи + <Style>

--- ToDo-List/ViewModels/ToDoViewModel.cs
+++ ToDo-List/ViewModels/ToDoViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 using ToDo_List.Models;
 
 namespace ToDo_List.ViewModels
@@ -11,6 +8,11 @@ namespace ToDo_List.ViewModels
     public class ToDoViewModel
     {
         public ObservableCollection<ToDoItem> Tasks { get; set; }
+
+        public string NewTaskTitle { get; set; }
+        public string NewTaskDescription { get; set; }
+
+        public ICommand AddTaskCommand { get; }
 
         public ToDoViewModel()
         {
@@ -20,6 +22,38 @@ namespace ToDo_List.ViewModels
                 new ToDoItem { Title = "Создать ToDo", Description = "Реализовать MVVM", IsCompleted = false },
                 new ToDoItem { Title = "Репетиторство", Description = "Провести репетиторство", IsCompleted = false }
             };
+
+            AddTaskCommand = new RelayCommand(AddTask);
         }
+
+        private void AddTask(object parameter)
+        {
+            if (!string.IsNullOrWhiteSpace(NewTaskTitle))
+            {
+                Tasks.Add(new ToDoItem { Title = NewTaskTitle, Description = NewTaskDescription, IsCompleted = false });
+
+                // Очищаем поля ввода
+                NewTaskTitle = string.Empty;
+                NewTaskDescription = string.Empty;
+            }
+        }
+    }
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) => _canExecute == null || _canExecute(parameter);
+
+        public void Execute(object parameter) => _execute(parameter);
     }
 }
