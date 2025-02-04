4c48d8a271ce969886cf5d58b21359c955c0d619
Polzovatel Dmitriy <root.polzovatel.dmitriy@gmail.com>
2025-02-04 18:09:10 +0300

    Создал MVVM модель/написал базовую логику

--- ToDo-List/MainWindow.xaml.cs
+++ ToDo-List/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDo_List.ViewModels;
 
 namespace ToDo_List
 {
@@ -23,6 +24,8 @@ namespace ToDo_List
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new ToDoViewModel();
         }
     }
 }
---
+++ ToDo-List/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDo_List.Models;
+
+namespace ToDo_List.ViewModels
+{
+    public class ToDoViewModel
+    {
+        public ObservableCollection<ToDoItem> Tasks { get; set; }
+
+        public ToDoViewModel()
+        {
+            Tasks = new ObservableCollection<ToDoItem>
+            {
+                new ToDoItem { Title = "Изучить WPF", Description = "Разобраться с XAML", IsCompleted = false },
+                new ToDoItem { Title = "Создать ToDo", Description = "Реализовать MVVM", IsCompleted = false },
+                new ToDoItem { Title = "Репетиторство", Description = "Провести репетиторство", IsCompleted = false }
+            };
+        }
+    }
+}
